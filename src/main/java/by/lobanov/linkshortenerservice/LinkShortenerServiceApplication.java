9f2c8010610cbf7e968ef11ae5f7bae299bdbec0
GitLobanov <lobanovgithub@gmail.com>
2025-07-22 13:21:52 +0300

    feat: Add initial project structure and README for link shortener service

---
+++ src/main/java/by/lobanov/linkshortenerservice/LinkShortenerServiceApplication.java
@@ -0,0 +1,13 @@
+package by.lobanov.linkshortenerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkShortenerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkShortenerServiceApplication.class, args);
+    }
+
+}
